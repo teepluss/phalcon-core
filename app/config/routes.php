25ad7e702a7ced22dddccf54a8edde29feb54e96
Teepluss <admin@jquerytips.com>
2015-05-28 14:00:44 +0700

    class

--- app/config/routes.php
+++ app/config/routes.php
@@ -3,14 +3,14 @@
 use Phalcon\Mvc\Router;
 use Phalcon\Http\Response;
 use Phalcon\Mvc\Router\Group as RouterGroup;
-use App\Config\Routes;
+//use App\Config\Routes;
 
 $router = new Router();
 $router->setDefaultModule('frontend');
 
 
 // Register routes
-$router->mount(new Routes\Frontend\Home);
-$router->mount(new Routes\Frontend\Blog);
+// $router->mount(new Routes\Frontend\Home);
+// $router->mount(new Routes\Frontend\Blog);
 
 return $router;
