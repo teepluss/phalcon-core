5a46d9ac67496329473ec5413adccf1cbce7cefd
Teepluss <admin@jquerytips.com>
2015-05-28 14:06:41 +0700

    class

--- app/config/loader.php
+++ app/config/loader.php
@@ -2,20 +2,23 @@
 
 $loader = new \Phalcon\Loader();
 
-$loader->registerNamespaces([
-    'App' => $config->application->appDir,
-    'Symfony\Component\VarDumper' => $config->application->vendorDir.'symfony/var-dumper/',
-]);
-
 /**
  * We're a registering a set of directories taken from the configuration file
  */
 $loader->registerDirs(
     array(
+        $config->application->appDir,
         $config->application->controllersDir,
         $config->application->modelsDir
     )
 );
+
+$loader->registerNamespaces([
+    'App' => $config->application->appDir,
+    'Symfony\Component\VarDumper' => $config->application->vendorDir.'symfony/var-dumper/',
+]);
+
+
 
 $loader->register();
 
