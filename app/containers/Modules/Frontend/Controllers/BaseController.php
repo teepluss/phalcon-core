0b305932621a20e319f1ed641fcad839af736a53
Watcharapol Tadtiang <wat.tadtiang@gmail.com>
2015-05-28 17:35:20 +0700

    finish template
    Signed-off-by: Watcharapol Tadtiang <wat.tadtiang@gmail.com>

--- app/containers/Modules/Frontend/Controllers/BaseController.php
+++ app/containers/Modules/Frontend/Controllers/BaseController.php
@@ -7,7 +7,20 @@ class BaseController extends Controller {
     public function initialize()
     {
     	$this->tag->prependTitle('Frontend | ');
-        $this->view->setTemplateAfter('main');
+        // $this->view->setTemplateAfter('common');
+
+		$this->assets
+		    ->collection('header')
+			->setPrefix('/')
+			->setLocal(false)
+		    ->addJs('js/jquery-1.11.3.min.js')
+		    ->addJs('bootstrap/js/bootstrap.min.js')
+		    ->addCss('bootstrap/css/bootstrap.min.css');
+
+		$this->assets
+		    ->collection('footer')
+			->setPrefix('/')
+			->setLocal(false);
     }
 
 }
