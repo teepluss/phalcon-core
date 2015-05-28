1aa2b012a8e52841b76156199943bd36126d9bdf
Teepluss <admin@jquerytips.com>
2015-05-28 14:34:33 +0700

    fix

---
+++ app/Modules/Frontend/Controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php namespace App\Modules\Frontend\Controllers;
+
+class IndexController extends BaseController {
+
+    public function indexAction()
+    {
+        //$this->events->fire('stock:increase', null, []);
+    }
+
+}
