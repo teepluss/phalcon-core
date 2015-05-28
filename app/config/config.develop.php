6262d4b5f427ad4f5e20ac6a142af439ad88a9e2
EThaiZone <ethaizone@hotmail.com>
2015-05-28 15:22:59 +0700

    Add env detector(2)

---
+++ app/config/config.develop.php
@@ -0,0 +1,12 @@
+<?php
+
+return new \Phalcon\Config(array(
+    'database' => array(
+        'adapter'     => 'Mysql',
+        'host'        => 'localhost',
+        'username'    => 'root',
+        'password'    => 'blabla',
+        'dbname'      => 'test',
+        'charset'     => 'utf8',
+    ),
+));
