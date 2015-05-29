601560d2a3589f1190d85eb8ccf6a93defa8106b
Watcharapol Tadtiang <wat.tadtiang@gmail.com>
2015-05-29 16:09:33 +0700

    config
    Signed-off-by: Watcharapol Tadtiang <wat.tadtiang@gmail.com>

--- app/config/config.php
+++ app/config/config.php
@@ -7,8 +7,8 @@ return new \Phalcon\Config(array(
         'adapter'     => 'Mysql',
         'host'        => 'localhost',
         'username'    => 'root',
-        'password'    => '',
-        'dbname'      => 'test',
+        'password'    => 'root',
+        'dbname'      => 'cpone',
         'charset'     => 'utf8',
     ),
     'application' => array(
